a241e3f3d931cd440023e6f9a457c38c16777741
ayazulhaqyousafzai <www.ayazkhan.com.21@gmail.com>
2025-06-11 21:57:45 +0500

    main application added

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from .auth import authenticate
+from .rag_engine import generate_response
+from .user_mgmt import router as user_router
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+
+os.environ['GROQ_API_KEY']=os.getenv('GROQ_API_KEY')
+
+app = FastAPI()
+app.include_router(user_router)
+
+llm = ChatGroq(model='gemma2-it')
+
+class QueryRequest(BaseModel):
+    email: str
+    password: str
+    query: str
+
+@app.post("/chat")
+def chat(request: QueryRequest):
+    role = authenticate(request.email, request.password)
+    if not role:
+        return {"error": "Unauthorized"}
+    response = generate_response(request.query, role, llm)
+    return {"response": response}
